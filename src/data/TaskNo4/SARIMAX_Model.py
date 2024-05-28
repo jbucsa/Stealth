cf192b2f12ac22cc7c6d9047fbc0c66319d212cc
jbucsa <justin.bucsa@gmail.com>
2024-05-28 18:23:39 -0500

    SARIMAX complete

---
+++ src/data/TaskNo4/SARIMAX_Model.py
@@ -0,0 +1,107 @@
+import yfinance as yf
+import pandas as pd
+import numpy as np
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+from statsmodels.tsa.statespace.sarimax import SARIMAX
+import warnings
+warnings.filterwarnings('ignore')
+import plotly.express as px
+
+tickers = ['MMM', 'ABT', 'ABBV', 'ABMD', 'ACN', 'ATVI', 'ADBE', 'AMD', 'AAP', 'AES', 'AFL', 'A', 'APD', 'AKAM', 'ALK', 'ALB', 'ARE', 'ALXN', 'ALGN', 'ALLE', 'LNT', 'ALL', 'GOOGL', 'GOOG', 'MO', 'AMZN', 'AMCR', 'AEE', 'AAL', 'AEP', 'AXP', 'AIG', 'AMT', 'AWK', 'AMP', 'ABC', 'AME', 'AMGN', 'APH', 'ADI', 'ANSS', 'ANTM', 'AON', 'AOS', 'APA', 'AAPL', 'AMAT', 'APTV', 'ADM', 'ANET', 'AJG', 'AIZ', 'T', 'ATO', 'ADSK', 'ADP', 'AZO', 'AVB', 'AVY', 'BKR', 'BLL', 'BAC', 'BK', 'BAX', 'BDX', 'BRK.B', 'BBY', 'BIO', 'BIIB', 'BLK', 'BA', 'BKNG', 'BWA', 'BXP', 'BSX', 'BMY', 'AVGO', 'BR', 'BF.B', 'CHRW', 'COG', 'CDNS', 'CPB', 'COF', 'CAH', 'KMX', 'CCL', 'CARR', 'CTLT', 'CAT', 'CBOE', 'CBRE', 'CDW', 'CE', 'CNC', 'CNP', 'CERN', 'CF', 'CRL', 'SCHW', 'CHTR', 'CVX', 'CMG', 'CB', 'CHD', 'CI', 'CINF', 'CTAS', 'CSCO', 'C', 'CFG', 'CTXS', 'CLX', 'CME', 'CMS', 'KO', 'CTSH', 'CL', 'CMCSA', 'CMA', 'CAG', 'CXO', 'COP', 'ED', 'STZ', 'COO', 'CPRT', 'GLW', 'CTVA', 'COST', 'CCI', 'CSX', 'CMI', 'CVS', 'DHI', 'DHR', 'DRI', 'DVA', 'DE', 'DAL', 'XRAY', 'DVN', 'DXCM', 'FANG', 'DLR', 'DFS', 'DISCA', 'DISCK', 'DISH', 'DG', 'DLTR', 'D', 'DPZ', 'DOV', 'DOW', 'DTE', 'DUK', 'DRE', 'DD', 'DXC', 'EMN', 'ETN', 'EBAY', 'ECL', 'EIX', 'EW', 'EA', 'EMR', 'ENPH', 'ETR', 'EOG', 'EFX', 'EQIX', 'EQR', 'ESS', 'EL', 'ETSY', 'EVRG', 'ES', 'RE', 'EXC', 'EXPE', 'EXPD', 'EXR', 'XOM', 'FFIV', 'FB', 'FAST', 'FRT', 'FDX', 'FIS', 'FITB', 'FE', 'FRC', 'FISV', 'FLT', 'FLIR', 'FLS', 'FMC', 'F', 'FTNT', 'FTV', 'FBHS', 'FOXA', 'FOX', 'BEN', 'FCX', 'GPS', 'GRMN', 'IT', 'GD', 'GE', 'GIS', 'GM', 'GPC', 'GILD', 'GL', 'GPN', 'GS', 'GWW', 'HAL', 'HBI', 'HIG', 'HAS', 'HCA', 'PEAK', 'HSIC', 'HSY', 'HES', 'HPE', 'HLT', 'HFC', 'HOLX', 'HD', 'HON', 'HRL', 'HST', 'HWM', 'HPQ', 'HUM', 'HBAN', 'HII', 'IEX', 'IDXX', 'INFO', 'ITW', 'ILMN', 'INCY', 'IR', 'INTC', 'ICE', 'IBM', 'IP', 'IPG', 'IFF', 'INTU', 'ISRG', 'IVZ', 'IPGP', 'IQV', 'IRM', 'JKHY', 'J', 'JBHT', 'SJM', 'JNJ', 'JCI', 'JPM', 'JNPR', 'KSU', 'K', 'KEY', 'KEYS', 'KMB', 'KIM', 'KMI', 'KLAC', 'KHC', 'KR', 'LB', 'LHX', 'LH', 'LRCX', 'LW', 'LVS', 'LEG', 'LDOS', 'LEN', 'LLY', 'LNC', 'LIN', 'LYV', 'LKQ', 'LMT', 'L', 'LOW', 'LUMN', 'LYB', 'MTB', 'MRO', 'MPC', 'MKTX', 'MAR', 'MMC', 'MLM', 'MAS', 'MA', 'MKC', 'MXIM', 'MCD', 'MCK', 'MDT', 'MRK', 'MET', 'MTD', 'MGM', 'MCHP', 'MU', 'MSFT', 'MAA', 'MHK', 'TAP', 'MDLZ', 'MPWR', 'MNST', 'MCO', 'MS', 'MOS', 'MSI', 'MSCI', 'NDAQ', 'NTAP', 'NFLX', 'NWL', 'NEM', 'NWSA', 'NWS', 'NEE', 'NLSN', 'NKE', 'NI', 'NSC', 'NTRS', 'NOC', 'NLOK', 'NCLH', 'NRG', 'NUE', 'NVDA', 'NVR', 'NXPI', 'ORLY', 'OXY', 'ODFL', 'OMC', 'OKE', 'ORCL', 'OTIS', 'PCAR', 'PKG', 'PH', 'PAYX', 'PAYC', 'PYPL', 'PNR', 'PBCT', 'PEP', 'PKI', 'PRGO', 'PFE', 'PM', 'PSX', 'PNW', 'PXD', 'PNC', 'POOL', 'PPG', 'PPL', 'PFG', 'PG', 'PGR', 'PLD', 'PRU', 'PTC', 'PEG', 'PSA', 'PHM', 'PVH', 'QRVO', 'PWR', 'QCOM', 'DGX', 'RL', 'RJF', 'RTX', 'O', 'REG', 'REGN', 'RF', 'RSG', 'RMD', 'RHI', 'ROK', 'ROL', 'ROP', 'ROST', 'RCL', 'SPGI', 'CRM', 'SBAC', 'SLB', 'STX', 'SEE', 'SRE', 'NOW', 'SHW', 'SPG', 'SWKS', 'SNA', 'SO', 'LUV', 'SWK', 'SBUX', 'STT', 'STE', 'SYK', 'SIVB', 'SYF', 'SNPS', 'SYY', 'TMUS', 'TROW', 'TTWO', 'TPR', 'TGT', 'TEL', 'TDY', 'TFX', 'TER', 'TSLA', 'TXN', 'TXT', 'TMO', 'TJX', 'TSCO', 'TT', 'TDG', 'TRV', 'TFC', 'TWTR', 'TYL', 'TSN', 'UDR', 'ULTA', 'USB', 'UAA', 'UA', 'UNP', 'UAL', 'UNH', 'UPS', 'URI', 'UHS', 'UNM', 'VLO', 'VAR', 'VTR', 'VRSN', 'VRSK', 'VZ', 'VRTX', 'VFC', 'VIAC', 'VTRS', 'V', 'VNO', 'VMC', 'WRB', 'WAB', 'WMT', 'WBA', 'DIS', 'WM', 'WAT', 'WEC', 'WFC', 'WELL', 'WST', 'WDC', 'WU', 'WRK', 'WY', 'WHR', 'WMB', 'WLTW', 'WYNN', 'XEL', 'XRX', 'XLNX', 'XYL', 'YUM', 'ZBRA', 'ZBH', 'ZION', 'ZTS']
+
+tickers = ['MMM', 'ABT', 'ABBV', 'ABMD', 'ACN', 'ATVI', 'ADBE', 'AMD', 'AAP', 'AES', 'AFL', 'A', 'APD', 'AKAM', 'ALK', 'ALB', 'ARE', 'ALXN', 'ALGN', 'ALLE']
+
+
+data = yf.download(tickers, start="2020-01-01", end="2024-05-27", group_by="ticker")
+dataNewer = yf.download(tickers, start="2022-01-01", end="2024-05-27", group_by="ticker")
+dataOlder = yf.download(tickers, start="2000-01-01", end="2024-05-27", group_by="ticker") 
+
+prices_dict = {}
+for ticker in tickers:
+        try:
+                prices_dict[ticker] = {}
+                df = data[ticker][['Close', 'Volume']]
+                prices_dict[ticker]["Now"] = df['Close'][-1]
+                model = SARIMAX(df['Close'], exog=df['Volume'], order=(1,2,1), seasonal_order=(1,1,1,12))
+                result = model.fit()
+                forecast_steps = 180
+                forecast = result.get_forecast(steps=forecast_steps, exog=df['Volume'][-forecast_steps:])
+                forecast_prices = forecast.predicted_mean
+                forecast_dates = pd.date_range(start=df.index[-1], periods=forecast_steps + 1, freq='D')
+                prices_dict[ticker]["Prediction"] = forecast_prices[521]
+        except:
+                pass
+            
+prices_dict_Older = {}
+for ticker in tickers:
+        try:
+                prices_dict_Older[ticker] = {}
+                df = dataOlder[ticker][['Close', 'Volume']]
+                prices_dict_Older[ticker]["Now"] = df['Close'][-1]
+                model = SARIMAX(df['Close'], exog=df['Volume'], order=(1,1,2), seasonal_order=(0,0,0,12))
+                result = model.fit()
+                forecast_steps = 180
+                forecast = result.get_forecast(steps=forecast_steps, exog=df['Volume'][-forecast_steps:])
+                forecast_prices = forecast.predicted_mean
+                forecast_dates = pd.date_range(start=df.index[-1], periods=forecast_steps + 1, freq='D')
+                prices_dict_Older[ticker]["Prediction"] = forecast_prices[-1]
+        except:
+                pass
+ 
+prices_dic_Newer = {}
+for ticker in tickers:
+        try:
+                prices_dict[ticker] = {}
+                df = dataNewer[ticker][['Close', 'Volume']]
+                prices_dict[ticker]["Now"] = df['Close'][-1]
+                model = SARIMAX(df['Close'], exog=df['Volume'], order=(1,1,2), seasonal_order=(0,0,0,12))
+                result = model.fit()
+                forecast_steps = 180
+                forecast = result.get_forecast(steps=forecast_steps, exog=df['Volume'][-forecast_steps:])
+                forecast_prices = forecast.predicted_mean
+                forecast_dates = pd.date_range(start=df.index[-1], periods=forecast_steps + 1, freq='D')
+                prices_dict[ticker]["Prediction"] = forecast_prices[-1]
+        except:
+                pass
+
+
+            
+prices_df = pd.DataFrame(prices_dict)
+predictions = prices_df.transpose().reset_index(drop=True)
+predictions["Ticker"] = prices_df.columns
+predictions = predictions.dropna()
+predictions["% Change"] = ((predictions["Prediction"] - predictions["Now"])/predictions["Now"]) *100
+top10 = predictions.sort_values(by="% Change",ascending=False)[:10]
+top10
+
+
+
+
+
+investments = {
+    'Seagate Technology Holdings plc (STX)': 25,
+    'Monolithic Power Systems, Inc. (MPWR)': 15,
+    'Jacobs Engineering Group Inc. (J)': 10,
+    '(GEN)': 10,
+    'The Estée Lauder Companies Inc. (EL)': 10,
+    'Enphase Energy, Inc. (ENPH)': 10,
+    '(MSCI)': 5,
+    'CVS Health Corporation (CVS)': 5,
+    'Incyte Corporation (INCY)': 5,
+    '(ADBE)': 5,
+}
+
+plt.figure(figsize=(10, 8))
+plt.pie(investments.values(), labels=investments.keys(), autopct='%1.1f%%', startangle=140)
+plt.title('Investment Allocation Based on Predictions')
+plt.axis('equal') 
+plt.show()
+
+
+
+
+
+
+
+
