571a647a729b9f5daac18806d886ddb3ebdbeaf9
jbucsa <justin.bucsa@gmail.com>
2024-06-05 09:59:57 -0500

    Added Factor Analysis

--- src/data/TaskNo4/PCA_StatisticalArbitrage.py
+++ src/data/TaskNo4/PCA_StatisticalArbitrage.py
@@ -38,7 +38,7 @@ stocks = yf.download('SPY,QQQ', start='2020-1-1', progress=False).Close
 stocks.head()
 
 # Equity Curve: PROD(1+R)-1
-stocks.pct_change().add(1).cumprod().add(-1).plot();
+stocks.pct_change().add(1).cumprod().add(-1).plot()
 
 
 # Equity Curve of Centered Data i.e. with mean return removed
@@ -61,3 +61,29 @@ eValues
 print('EigenVectors - Each column is an EigenVector, look closely they are perpendicular!')
 eVectors=E[1]
 eVectors
+
+# Plot the Correlation of Stock Centered Returns
+# 1. Compute Data for Principal Component Lines
+x_axis = np.linspace(np.nanmin(centered_returns.SPY), np.nanmax(centered_returns.SPY))
+dx1 = eVectors[0,0]
+dy1 = eVectors[1,0]
+slope1 = dy1/dx1
+dx2 = eVectors[0,1]
+dy2 = eVectors[1,1]
+slope2 = dy2/dx2
+y_axis1 = slope1 * x_axis
+y_axis2 = slope2 * x_axis
+# 2. Plot Centered Return Correlation Between X and Y
+plt.plot(centered_returns.SPY, centered_returns.QQQ, '.', alpha=0.25)
+# 3. Plot Principal Components
+plt.plot(x_axis, y_axis1, label='first component')
+plt.plot(x_axis, y_axis2, label='second component')
+plt.legend()
+plt.xlabel('R0 Values')
+plt.ylabel('R1 Values')
+plt.show()
+
+
+# Principal Components and Explained Variance
+# Downloading More Instruments
+equities = yf.download('GOOG,AMZN,NFLX,BAC,JPM,WFC', start='2020-1-1', progress=False).Close
